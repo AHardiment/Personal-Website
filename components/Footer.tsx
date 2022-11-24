0ea39d335afa77eeafba076259e8f73116adf799
AHardiment <99344068+AHardiment@users.noreply.github.com>
2022-11-24 00:22:29 +0000

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,8 +6,8 @@ const Footer = () => {
   return (
     <div className=" mt-32 flex items-end justify-center">
       <p className="text-center text-xs text-gray-500 md:text-sm">
-        This site is a work in progress. Please bear with me as I continue to
-        add content.
+        This site was built with Next.JS, TypeScript and TailwindCSS. It is a
+        work in progress so please bear with me as I continue to add content.
       </p>
     </div>
   );
