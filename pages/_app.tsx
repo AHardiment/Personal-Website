90a2aa258f44aeab11cd43af2fc79077d67c148a
AHardiment <99344068+AHardiment@users.noreply.github.com>
2022-11-26 10:05:37 +0000

    added viewer analytics component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
