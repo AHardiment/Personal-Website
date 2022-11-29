8e9ed9224ee798c7c63183679cc531045d2996aa
AHardiment <99344068+AHardiment@users.noreply.github.com>
2022-11-29 09:53:28 +0000

    more mobile nav dev
    styled about page better on mobile devices

--- components/AboutDetails.tsx
+++ components/AboutDetails.tsx
@@ -1,24 +1,35 @@
 import React from "react";
 import Carousel from "./Carousel";
+import Image from "next/image";
+import carouselImg2 from "../public/casual-img.jpg";
 
 const AboutDetails = () => {
   return (
     <div className="about-details m-0 mx-auto h-full max-w-5xl md:mt-10">
       <div className="">
-        <h1 className="font-bold md:text-6xl">About Me:</h1>
-        <p className="text-justify md:mt-5 md:text-2xl">
+        <h1 className="mx-5 mt-3 text-3xl font-bold md:text-6xl">About Me:</h1>
+        <p className="mx-5 mt-5 text-justify text-xl md:mt-5 md:text-2xl">
           My name is Alex Hardiment, and I am a full-stack web developer based
           in the UK.
         </p>
-        <p className="text-justify md:mt-3 md:text-2xl">
-          Earlier this year, I rediscovered my passion for web development and
-          decided that this would be the moment I gave it my all.
+        <p className="mx-5 mt-3 text-justify text-xl md:mt-3 md:text-2xl">
+          Earlier this year, I rediscovered my passion for software development
+          and decided that this would be the moment I gave it my all.
         </p>
-        <p className="text-justify md:mt-3 md:text-2xl">
+        <p className="mx-5 mt-3 text-justify text-xl md:mt-3 md:text-2xl">
           This site is a glimpse at that journey.
         </p>
       </div>
-      <Carousel />
+      <div>
+        <Image
+          src={carouselImg2}
+          className="mx-auto mt-10 block aspect-auto w-11/12 rounded-lg md:hidden"
+          alt="Picture of Alex at restaurant"
+        />
+      </div>
+      <div className="mx-5 mt-10 hidden md:block">
+        <Carousel />
+      </div>
     </div>
   );
 };
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className=" mx-5 mt-32 flex items-end justify-center">
+    <div className="mx-5 mt-16 flex items-end justify-center lg:mt-32">
       <p className="text-center text-xs text-gray-500 md:text-sm">
         This site was built with Next.JS, TypeScript and TailwindCSS. It is a
         work in progress so please bear with me as I continue to add content.
